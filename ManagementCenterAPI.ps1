e5043e48aa5d12defec3bf952bdb8b65504fdc57
krgladden <30503690+krgladden@users.noreply.github.com>
2019-07-24 15:37:52 -0400

    Update ManagementCenterAPI.ps1

--- ManagementCenterAPI.ps1
+++ ManagementCenterAPI.ps1
@@ -1,33 +1,53 @@
+Function Query-ManagementCenter{
+[cmdletbinding()]
+param(
+[parameter(Mandatory,
+ValueFromPipeline)]
+[string[]]
+$MCSite,
 
-#The following lines are to disable SSL certificate validation, since the proxies all have self signed certificates
-add-type @"
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
-public class TrustAllCertsPolicy : ICertificatePolicy {
-    public bool CheckValidationResult(
-        ServicePoint srvPoint, X509Certificate certificate,
-        WebRequest request, int certificateProblem) {
-        return true;
+[parameter(ValueFromPipeline)]
+[int]
+$port=8082,
+
+
+[parameter(Mandatory,
+ValueFromPipeline)]
+[hashtable]$APIHeader
+
+
+
+[parameter(Mandatory,
+ValueFromPipeline)]
+[validateset("Devices","System","Jobs")]
+[string[]]
+$APIcall,
+
+
+
+
+)
+process{
+
+
+
+Switch ($APIcall){
+        "Devices" {$APIset = 'devices/health'
+                  }
+        "System"  {$APIset = 'system/version'
+                  }
+         "Jobs"   {$APIset = 'jobs'
+                  }
+    }	
+
+write-verbose "$MCSite(s) selected."
+write-verbose "$APIcall check called."
+
+Foreach ($mc in $MCsite){
+write-host "$($mc) being queried."
+$APIData = Invoke-restmethod -Uri "https://$($MC):$($port)/api/$APIset" -Headers $APIHeader -contenttype "application/json"
+$DataCalled += $APIdata
+        }
+$DataCalled
     }
 }
-"@
-[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-
-$MCSite = read-host "Select Management Center Site. Input as nrfk, sdni, or all"
-
-
-write-host $mcsite1 $mcsite2
-$cred = Get-Credential -Message "Please input Username and Password"
-$pw = $cred.GetNetworkCredential().Password
-$user = $cred.UserName
-$pair = "${user}:${pw}"
-$bytes = [System.text.encoding]::ASCII.GetBytes($pair)
-$base64 = [System.Convert]::tobase64string($bytes)
-$BasicAuthValue = "Basic $base64"
-$headers = @{ Authorization = $BasicAuthValue }
-
-
-
-$testing = Invoke-restmethod -Uri 'https://***:8082/api/devices/health' -Headers $headers -contenttype "application/json"
-
-$testing | select-object @{Name = "Hostname"; Expression =  {$_.name}} -ExpandProperty health
